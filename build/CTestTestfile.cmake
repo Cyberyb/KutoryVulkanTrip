1fd2eb634ed881c5dc21feeb28bf5b047da8c2db
Cybery <cyberybhshu@163.com>
2024-05-16 00:00:37 +0800

    remove bulid and .vscode

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Myroot/vcprojects/KutoryVulkanTrip
-# Build directory: C:/Myroot/vcprojects/KutoryVulkanTrip/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
